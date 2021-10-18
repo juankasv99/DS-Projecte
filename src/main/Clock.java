dbd547a3f4362513f67d30d13bcdd70670a27cfe
Frank Calderón Molina <frankcalderonmolina@gmail.com>
2021-10-18 19:15:51 +0200

    Create Clock functionality

--- src/main/Clock.java
+++ src/main/Clock.java
@@ -1,13 +1,28 @@
 package main;
 
+import java.time.LocalDateTime;
 import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Clock extends Observable {
+
   private static Clock uniqueInstance;
+  private Timer timer;
+  private TimerTask timerTask;
 
-  private Clock() {}
+  private Clock() {
+    this.timer = new Timer("Timer");
+    this.timerTask = new TimerTask() {
+      @Override
+      public void run() {
+        tick();
+      }
+    };
+    this.timer.scheduleAtFixedRate(timerTask, 0, 1000);
+  }
 
-  public Clock getInstance() {
+  public static Clock getInstance() {
     if (uniqueInstance == null) {
       uniqueInstance = new Clock();
     }
@@ -15,7 +30,7 @@ public class Clock extends Observable {
     return uniqueInstance;
   }
 
-  public void tick() {
+  private void tick() {
     setChanged();
     notifyObservers();
   }
--- src/main/Interval.java
+++ src/main/Interval.java
@@ -1,11 +1,44 @@
 package main;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Interval implements Observer {
+
+  private LocalDateTime startTime;
+  private LocalDateTime endTime;
+  private Duration duration;
+  private Task task;
+
+  public Interval(Task task) {
+    this.startTime = LocalDateTime.now();
+    this.duration = Duration.ZERO;
+    this.task = task;
+    Clock.getInstance().addObserver(this);
+  }
+
   @Override
   public void update(Observable o, Object arg) {
+    this.duration = this.duration.plusSeconds(1);
+    this.endTime = this.startTime.plusSeconds(1);
 
+    /*
+    if (this.startTime == null) {
+      this.startTime = currentTime;
+      task.setStartTime(this.startTime);
+    }
+    this.duration = Duration.between(this.startTime, currentTime);
+    this.endTime = currentTime;
+    this.task.componentDuration();
+    this.task.setEndTime(this.endTime);
+    Printer.getInstance(null).print();
+    */
+  }
+
+  @Override
+  public String toString() {
+    return "";
   }
 }
