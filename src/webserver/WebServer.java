2317826aeebf6ca65b8505f1573a66696d11fab7
Frank Calderón Molina <31405512+frcalderon@users.noreply.github.com>
2022-01-02 18:47:14 +0100

    Add "log" in add Project/Task endpoint.

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // Based on
@@ -146,6 +147,7 @@ public class WebServer {
           break;
         }
         case "add": {
+          System.out.println(Arrays.toString(tokens));
           break;
         }
         case "last": {
